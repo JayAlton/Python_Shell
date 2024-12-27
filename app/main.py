7f75ebf1de7c532d3eed71281210772fa12fbcc1
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-26 20:47:49 -0800

    Echo

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 import sys
-
+import 
 
 def main():
     while True:
         sys.stdout.write("$ ")
+        sys.stdout.flush()
 
         # Wait for user input
         command = input()
-        if(command == "exit 0"):
-            break
-        
-        print(f"{command}: command not found")
+        if (command == "exit 0"):
+            sys.exit(0)
+        elif (command.startswith("echo ")):
+            print(command[len("echo ") :])
+        else:
+            print(f"{command}: command not found")
 
 
 
