d00bf3bfcf111fb672c14db506ae962f2e761c50
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-26 21:10:44 -0800

    type builtin

--- app/main.py
+++ app/main.py
@@ -1,26 +1,39 @@
 import sys 
+import os
 
 def main():
+    commands = {"exit", "echo", "type"}
+    PATH = os.environ.get("PATH")
+    
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        commands = {"exit", "echo", "type"}
-
-        
         # Wait for user input
         userInput = input()
+
         if (userInput == "exit 0"):
             sys.exit(0)
+        
         elif (userInput.startswith("echo ")):
             print(userInput[len("echo ") :])
+        
         elif (userInput.startswith("type ")):
-            args = userInput.split()
-            if len(args) == 2 and args[1] in commands:
+            args = userInput.split(" ")[1]
+            cmd_path = None
+            paths = PATH.split(":")
+            for path in paths:
+                if os.path.isfile(f"{path}/{args}"):
+                    cmd_path = f"{path}/{args}"
+            if args in commands:
                 print(f"{args[1]} is a shell builtin")
+            elif cmd_path:
+                print(f"{args} is {cmd_path}\n")
             else:
                 print(f"{args[1]}: not found")          
         else:
             print(f"{userInput}: command not found")
+        
+        sys.stdout.flush()
 
 
 
