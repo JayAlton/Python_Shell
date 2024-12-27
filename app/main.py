d486454156ec485ef83326f917e6e726bfccfe39
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-26 21:31:04 -0800

    type builtin exec

--- app/main.py
+++ app/main.py
@@ -1,48 +1,51 @@
 import sys 
 import os
-import shutil
+import subprocess
+
+from typing import Optional
 
 def find_executable(command, path):
+    PATH = os.environ.get("PATH", "")
+
     directories = path.split(":")
     for dir in directories:
         file_path = os.path.join(dir, command)
         if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
             return file_path
     return None
+
+def handle_exit(args):
+    sys.exit(int(args[0]) if args else 0)
+
+def handle_echo(args):
+    print(" ".join(args))
+
+def handle_type(args):
+    if args[0] in builtins:
+        print(f"{args[0]} is a shell builtin")
+    elif executable := find_executable(args[0]):
+        print(f"{args[0]} is {executable}")
+    else:
+        print(f"{args[0]} not found")
+
+builtins = { "exit": handle_exit, "echo": handle_echo, "type": handle_type}
+
 def main():
-    commands = {"exit", "echo", "type"}
-    PATH = os.environ.get("PATH", "")
-    
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # Wait for user input
-        userInput = input()
+        userInput, *args = input().split(" ")
 
-        if (userInput == "exit 0"):
-            sys.exit(0)
-        
-        elif (userInput.startswith("echo ")):
-            print(userInput[len("echo ") :])
-        
-        elif (userInput.startswith("type ")):
-            args = userInput.split()
-            if len(args) == 2:
-                if args[1] in commands:
-                    print(f"{args[1]} is a shell builtin")
-                else:
-                    executable_path = find_executable(args[1], PATH)
-                    if executable_path:
-                        print(f"{args[1]} is {executable_path}")
-                    else:
-                        print(f"{args[1]}: not found")
-            else:
-                print(f"type: expected a command")          
-        
+        if userInput in builtins:
+            builtins[userInput](args)
+            continue
+        elif executable := find_executable(userInput):
+            subprocess.run([executable, *args])   
         else:
             print(f"{userInput}: command not found")
 
-
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
