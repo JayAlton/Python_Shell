30d637513e8849157d98c1187aa525fbd16c97e7
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-30 00:36:36 -0800

    Single and double quotes

--- app/main.py
+++ app/main.py
@@ -24,6 +24,8 @@ def handle_exit(args):
     sys.exit(int(args[0]) if args else 0)
 
 def handle_echo(args):
+    if isinstance(args, list):
+        args = " ".join(args)
     result = []
     for i in args:
         if (i.startswith("'") and i.endswith("'")) or (i.startswith('"') and i.endswith('"')):
